ac663f93f027032d9f9d342c2a96913a857d29af
Rakeshreddy-07 <rakeshreddynarra07@gmail.com>
2025-04-18 14:24:52 +0530

    roboshop infra

--- 70-ecr/main.tf
+++ 70-ecr/main.tf
@@ -15,3 +15,33 @@ resource "aws_ecr_repository" "frontend" {
     }
   
 }
+
+resource "aws_ecr_repository" "mongodb" {
+    name = "expense/mongodb"
+    image_tag_mutability = "MUTABLE"
+    image_scanning_configuration {
+      scan_on_push = true 
+    }
+  
+}
+
+
+
+resource "aws_ecr_repository" "catalogue" {
+    name = "roboshop/catalogue"
+    image_tag_mutability = "MUTABLE"
+    image_scanning_configuration {
+      scan_on_push = true 
+    }
+  
+}
+
+
+resource "aws_ecr_repository" "catalogue" {
+    name = "roboshop/user"
+    image_tag_mutability = "MUTABLE"
+    image_scanning_configuration {
+      scan_on_push = true 
+    }
+  
+}
